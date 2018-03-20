f277e24a0f212cb6ae08f44ed6315f8385f9d989
Balou9 <emmanuel.owosekun@gmail.com>
2018-03-20 22:39:50 +0100

    fixed usage

--- usage.js
+++ usage.js
@@ -3,4 +3,4 @@ var astring = 'wefiwejifljwie\\\\\///////\\\\/////'
 
 var obj = slashDetector(astring)
 
-console.log(obj.slash.length, obj.backslash.length, astr.length, obj)
+console.log(obj.slash.length, obj.backslash.length, astring.length, obj)
